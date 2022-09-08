1dee5124ae61839f6e59f9eebe2beefb457fc7e5
LemonHX <lemonhx@lemonhx.tech>
2022-09-08 23:02:10 +0800

    transport 更透明

--- transport.py
+++ transport.py
@@ -1,69 +1,18 @@
+import json
+
+trans = json.load(open("transport.json", "r"))
 def pinyin_trans(pinyin):
-    if pinyin == "iao":
-        return "q"
-    if pinyin == "en":
-        return "w"
-    if pinyin == "eng" or pinyin == "uan":
-        return "r"
-    if pinyin == "ue" or pinyin == "ve":
-        return "t"
-    if pinyin == "un":
-        return "y"
-    if pinyin == "uo":
-        return "u"
-    if pinyin == "ou":
-        return "o"
-    if pinyin == "ie":
-        return "p"
-    if pinyin == "uang":
-        return "s"
-    if pinyin == "ai":
-        return "d"
-    if pinyin == "an":
-        return "f"
-    if pinyin == "ang":
-        return "g"
-    if pinyin == "ao":
-        return "h"
-    if pinyin == "ian":
-        return "j"
-    if pinyin == "ing" or pinyin == "uai":
-        return "k"
-    if pinyin == "in":
-        return "l"
-    if pinyin == "ei":
-        return "z"
-    if pinyin == "ia":
-        return "x"
-    if pinyin == "ua":
-        return "c"
-    if pinyin == "ui":
-        return "v"
-    if pinyin == "iu":
-        return "b"
-    if pinyin == "ong" or pinyin == "iong":
-        return "n"
-    if pinyin == "iang":
-        return "m"
-    if pinyin == "zh":
-        return "i"
-    if pinyin == "ch":
-        return "v"
-    if pinyin == "sh":
-        return "u"
-    return pinyin
+    if pinyin in trans["pinyin"].keys():
+        return trans["pinyin"][pinyin]
+    else:
+        return pinyin
 
 
 def tone_trans(tone):
-    if tone == "1":
-        return "f"
-    if tone == "2":
-        return "g"
-    if tone == "3":
-        return "h"
-    if tone == "4":
-        return "j"
-    return "b"
+    if tone in trans["tone"].keys():
+        return trans["tone"][tone]
+    else:
+        return "b"
 
 if __name__ == "__main__":
     import sys
